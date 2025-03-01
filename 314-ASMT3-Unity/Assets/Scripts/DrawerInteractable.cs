280a11cc0916cf26c0d5d9cf41a0abcafb9ad5a4
Grace Fowler <f.llfr.mgrace@gmail.com>
2025-02-28 22:20:25 -0500

    Customized drawer grab interactions

--- 314-ASMT3-Unity/Assets/Scripts/DrawerInteractable.cs
+++ 314-ASMT3-Unity/Assets/Scripts/DrawerInteractable.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class DrawerInteractable : MonoBehaviour
+public class DrawerInteractable : XRGrabInteractable
 {
     [SerializeField] XRSocketInteractor keySocket;
-
     [SerializeField] bool isLocked;
+
+    Transform parentTransform;
+
+    private const string defaultLayer = "Default";
+    private const string grabLayer = "Grab";
 
     void Start()
     {
@@ -17,6 +21,7 @@ public class DrawerInteractable : MonoBehaviour
             keySocket.selectEntered.AddListener(OnDrawerUnlocked);
             keySocket.selectExited.AddListener(OnDrawerLocked);
         }
+        parentTransform = transform.parent.transform;
     }
 
     private void OnDrawerLocked(SelectExitEventArgs arg0)
@@ -31,8 +36,22 @@ public class DrawerInteractable : MonoBehaviour
         Debug.Log("Drawer unlocked");
     }
 
-    void Update()
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        
+        base.OnSelectEntered(args);
+        if (!isLocked)
+        {
+            transform.SetParent(parentTransform);
+        }
+        else
+        {
+            interactionLayers = InteractionLayerMask.GetMask(defaultLayer);
+        }
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        interactionLayers = InteractionLayerMask.GetMask(grabLayer);
     }
 }
